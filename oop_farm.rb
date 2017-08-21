bc2c288e77b334ba5acc5d79050c44b0575a4377
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-20 23:59:38 -0400

    added harvest,status,and relax methods

--- oop_farm.rb
+++ oop_farm.rb
@@ -6,6 +6,7 @@ class Farm
     @name = name
     @corn = []
     @wheat = []
+    @food_harvested = 0
   end
 
   def main_menu
@@ -29,9 +30,9 @@ class Farm
   def call_option(user_selected)
     case user_selected
     when 'field' then adds_new_field
-    # when harvest then harvests_crops_adds_to_total
-    # when status then display_info_about_farm
-    # when relax then description_of_farm
+    when 'harvest' then harvests_crops_adds_to_total
+    when 'status' then display_info_about_farm
+    when 'relax' then description_of_farm
     when 'exit' then abort("Thank you for playing!")
     else
       puts "#{user_selected}is not an valid option"
@@ -50,6 +51,55 @@ class Farm
         @wheat << hectares_size
       end
     end
+
+    def harvests_crops_adds_to_total
+      sum = 0
+      @corn.each do |hectares|
+        sum += hectares
+      end
+      total_corn = (sum * 20)
+      if sum > 0
+      puts "Harvesting #{total_corn} food from #{sum} corn field."
+      end
+        total = 0
+      @wheat.each do |hectares|
+        total += hectares
+      end
+      total_wheat = (total * 30)
+      if total > 0
+        puts "Harvesting #{total_wheat} food from #{total} wheat field."
+      end
+        @food_harvested += total_corn + total_wheat
+    puts "The farm has #{@food_harvested} food so far."
+    end
+
+    def display_info_about_farm
+      sum = 0
+      @corn.each do |hectares|
+        sum += hectares
+      end
+      puts "Corn field is #{sum} hectares."
+      total = 0
+    @wheat.each do |hectares|
+      total += hectares
+    end
+    puts "Wheat field is #{total} hectares."
+    puts "The farm has #{@food_harvested} harvested food so far."
+    end
+
+    def description_of_farm
+      sum = 0
+      @corn.each do |hectares|
+        sum += hectares
+      end
+      puts "Corn field is #{sum} hectares."
+      total = 0
+    @wheat.each do |hectares|
+      total += hectares
+      end
+      puts "#{sum} hectares of tall green stalks rustling in the breeze fill your horizon. The sun hangs low,casting an organe glow on a sea of #{total} hectares of wheat."
+    end
+
 
 
 end
